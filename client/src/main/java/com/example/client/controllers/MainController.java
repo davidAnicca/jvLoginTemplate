d60ac364f1adbc52fb98822f08dd032afe51cc50
anicca <meresan02david1006@gmail.com>
2023-06-21 16:16:48 +0300

    logout

--- client/src/main/java/com/example/client/controllers/MainController.java
+++ client/src/main/java/com/example/client/controllers/MainController.java
@@ -1,13 +1,19 @@
 package com.example.client.controllers;
 
+import com.example.client.HelloApplication;
 import com.example.client.controllers.content.Content;
 import com.example.client.services.Service;
 import javafx.application.Platform;
 import javafx.collections.ObservableList;
 import javafx.concurrent.Task;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
 
 
 public class MainController {
@@ -42,5 +48,20 @@ public class MainController {
 
     public void startGame(ActionEvent event) {
 
+    }
+
+    public void logout(ActionEvent event) throws Exception {
+        listenThread.interrupt();
+        FXMLLoader loader = new FXMLLoader(HelloApplication.class.getResource("/login-view.fxml"));
+        AnchorPane root = loader.load();
+        Scene scene = new Scene(root,root.getPrefWidth(), root.getPrefHeight());
+        Stage stage = new Stage();
+        stage.setTitle("Login");
+        stage.setScene(scene);
+        stage.show();
+        LoginController controller = loader.getController();
+        controller.initialize(new Service());
+        Stage currentStage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        currentStage.close();
     }
 }
